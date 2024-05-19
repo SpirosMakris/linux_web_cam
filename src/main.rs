1dfd716a4b600eac5aa0e437d66e9eb70d262acb
Spiros Makris <smakris@iit.demokritos.gr>
2024-05-19 12:25:59 +0300

    got capabilities

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::{fs::OpenOptions, mem::MaybeUninit, os::fd::AsRawFd};
+
+mod v4l2;
+use v4l2::*;
+
+const DEVICE_NAME: &str = "/dev/video0";
+
+// Generated with `resolve_ioctl.c``
+const VIDIOC_QUERYCAP: u64 = 2154321408;
+
 fn main() {
-    println!("Hello, world!");
+    // Open device file
+    let video_handle = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .open(DEVICE_NAME)
+        .unwrap();
+
+    // Get device capabilities
+    let fd = video_handle.as_raw_fd();
+
+    let capabilities = unsafe {
+        let mut capabilities: MaybeUninit<v4l2_capability> = MaybeUninit::uninit();
+        v4l2::ioctl(fd, VIDIOC_QUERYCAP, capabilities.as_mut_ptr());
+        capabilities.assume_init()
+    };
+
+    println!("{capabilities:?}");
 }
