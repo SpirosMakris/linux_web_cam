c152640badfc4934baa52ac1f633cc21aebafc20
Spiros Makris <smakris@iit.demokritos.gr>
2024-05-22 11:03:45 +0300

    split out thread to feed images

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,52 @@
-use eframe::egui::{self, Color32, ColorImage, TextureOptions};
-use std::{fs::OpenOptions, io::Write};
+use eframe::egui::{self, Color32, ColorImage, TextureHandle, TextureOptions};
+use std::sync::mpsc::{self, Receiver, Sender};
 
 mod v4l2;
 
 const DEVICE_NAME: &str = "/dev/video0";
 
 struct WebcamUi {
-    v4l2_device: v4l2::V4l2VideoDevice,
+    rx: Receiver<TextureHandle>,
+    last_texture: Option<TextureHandle>,
 }
 
 impl WebcamUi {
-    fn new(_cc: &eframe::CreationContext<'_>) -> Self {
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
         // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
         // Restore app state using cc.storage (requires the "persistence" feature).
         // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
         // for e.g. egui::PaintCallback.
         let v4l2_device = v4l2::V4l2VideoDevice::new(&DEVICE_NAME);
+        let (tx, rx) = mpsc::channel();
+        let ctx = cc.egui_ctx.clone();
 
-        WebcamUi { v4l2_device }
+        std::thread::spawn(move || feed_gui(ctx, v4l2_device, tx));
+
+        WebcamUi {
+            rx,
+            last_texture: None,
+        }
     }
 }
 
 impl eframe::App for WebcamUi {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        let v4l2_frame = self.v4l2_device.get_frame();
+        while let Ok(v) = self.rx.try_recv() {
+            self.last_texture = Some(v);
+        }
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            if let Some(texture) = &self.last_texture {
+                ui.image((texture.id(), texture.size_vec2()));
+            }
+            ui.heading("Hello World!");
+        });
+    }
+}
+
+fn feed_gui(ctx: egui::Context, v4l2_device: v4l2::V4l2VideoDevice, tx: Sender<TextureHandle>) {
+    loop {
+        let v4l2_frame = v4l2_device.get_frame();
 
         // YUYV encoded
         let data = v4l2_frame.data();
@@ -42,11 +65,8 @@ impl eframe::App for WebcamUi {
         };
 
         let texture = ctx.load_texture("our only image", image, TextureOptions::LINEAR);
-
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.image((texture.id(), texture.size_vec2()));
-            ui.heading("Hello World!");
-        });
+        tx.send(texture).unwrap();
+        ctx.request_repaint();
     }
 }
 
@@ -58,18 +78,4 @@ fn main() {
         Box::new(|cc| Box::new(WebcamUi::new(cc))),
     )
     .unwrap();
-}
-
-fn __main() {
-    let device = v4l2::V4l2VideoDevice::new(&DEVICE_NAME);
-
-    let frame = device.get_frame();
-
-    let mut output = OpenOptions::new()
-        .write(true)
-        .create(true)
-        .truncate(true)
-        .open("test_2.yuv")
-        .unwrap();
-    output.write_all(frame.data()).unwrap();
 }
